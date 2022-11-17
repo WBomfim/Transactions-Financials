e15130faf16a97df7ed7028a506ed1814218f865
WBomfim <willianbomfim@hotmail.com>
2022-11-17 17:54:48 -0300

    feat: Criado a migration create-Users

---
+++ backend/src/database/migrations/20221117174016-create-Users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      accountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Users');
+  }
+};
