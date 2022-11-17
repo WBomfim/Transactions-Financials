e8b184281e160847cc61f968ee2a0755d76e4601
WBomfim <willianbomfim@hotmail.com>
2022-11-17 14:04:21 -0300

    feat: Criado o conexão do sequelize como banco de dados

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -1,6 +1,16 @@
-import { App } from './app';
 import 'dotenv/config';
+import * as express from 'express';
+import sequelize from '../database/models';
 
 const PORT = process.env.APP_PORT || 3001;
 
-new App().start(PORT);
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  }).catch((error: any) => {
+    console.error('Unable to connect to the database:', error);
+  });
+});
---
+++ backend/src/database/config/database.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: 'postgres',
+};
+
+module.exports = config;
