f88e4f79eae31238544814c428b4349b0b2b284c
WBomfim <willianbomfim@hotmail.com>
2022-11-18 20:13:59 -0300

    feat: Criado um enum para os status http

---
+++ backend/src/types/StatusHttp.ts
@@ -0,0 +1,11 @@
+enum StatusHttp {
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  INTERNAL_ERROR = 500,
+}
+
+export default StatusHttp;
