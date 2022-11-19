905193b5cec51dfb36f52feb8c76ea2d66d36878
WBomfim <willianbomfim@hotmail.com>
2022-11-19 00:17:40 -0300

    chore: Personalizadas as mensagens de erro emitidas na validação dos dados de usuário

--- backend/src/types/User.ts
+++ backend/src/types/User.ts
@@ -2,8 +2,12 @@ import { z } from 'zod';
 
 const userSchema = z.object({
   id: z.number(),
-  username: z.string().min(3),
-  password: z.string().min(8).regex(/[A-Z]/).regex(/[0-9]/),
+  username: z.string({ required_error: 'username is required' })
+    .min(3, { message: 'username must be at least 3 characters long' }),
+  password: z.string({ required_error: 'password is required' })
+    .min(8, { message: 'password must be at least 8 characters long' })
+    .regex(/[A-Z]/, { message: 'password must have at least one upper case letter' })
+    .regex(/[0-9]/, { message: 'password must have at least one numeric character' }),
   accountId: z.number(),
 });
 
