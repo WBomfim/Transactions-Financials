d77e55c836d2a1fca2cf5e71f17edaf8bf828800
WBomfim <willianbomfim@hotmail.com>
2022-11-19 12:57:32 -0300

    refactor: Retirado a verificação de dados no login - Avaliado que não há necessidade dessa validação, uma vez que as informações serão comparadas com as cadastradas no banco

--- backend/src/services/login.ts
+++ backend/src/services/login.ts
@@ -8,7 +8,6 @@ import { generateToken } from '../helpers/handleToken';
 
 const login = async (loginObj: User): Promise<TokenReturn> => {
   const { username, password } = loginObj;
-  validateUserData(loginObj);
 
   const user = await UserModel.findOne({ where: { username } });
   if (!user) throw new Error(ErrorsTypes.INVALID_USERNAME);
