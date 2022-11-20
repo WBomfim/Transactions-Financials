7cd25ca600f183c4bb918f58f8b5f90810ac7c28
WBomfim <willianbomfim@hotmail.com>
2022-11-20 01:40:44 -0300

    feat: Criado a estrutura das páginas e definido as rotas para cada uma

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hellooooooooooo</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Navigate to="/login" /> } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/registration" element={ <Registration /> } />
+      <Route path="/home" element={ <Home /> } />
+      <Route path="*" element={ <Navigate to="/" /> } />
+    </Routes>
   );
 }
-
-export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
