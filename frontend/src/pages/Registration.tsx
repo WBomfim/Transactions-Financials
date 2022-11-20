2fb0843d664aa28cd967d7cf474a8707b62762f3
WBomfim <willianbomfim@hotmail.com>
2022-11-20 03:59:07 -0300

    feat: Criado a página de cadastro do usuário - Criado a página com os imputs e as validações necessárias para entrada das informações

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -1,7 +1,64 @@
-export default function Registration() {
+import { useState, useEffect } from 'react';
+
+export default function Registration(): JSX.Element {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [disabledButton, setDisabledButton] = useState<boolean>(true);
+  const [failedTryRegister, setFailedTryRegister] = useState<boolean>(false);
+
+  useEffect(() => {
+    const verifyRegisterData = () => {
+      const nameLengthMin = 3;
+      const passwordLengthMin = 8;
+      const errors = [
+        !username || username.length < nameLengthMin,
+        !password || password.length < passwordLengthMin,
+        !password ||
+          !password.match(/(?=.*[A-Z])(?=.*[0-9]).*$/),
+      ];
+      const hasErrors = errors.some((error) => error);
+      setDisabledButton(hasErrors);
+    };
+    verifyRegisterData();
+  }, [username, password]);
+
   return (
-    <div>
-      <h1>Registration Aquii!!</h1>
-    </div>
+    <main>
+      <form>
+        <label htmlFor='name-input'>
+          Nome:
+          <input
+            id='name-input'
+            type='text'
+            onChange={({ target: { value } }) => {
+              setUsername(value);
+            }}
+            placeholder='Marcelo da Silva'
+          />
+        </label>
+        <label htmlFor='password-input'>
+          Senha:
+          <input
+            id='password-input'
+            type='text'
+            onChange={({ target: { value } }) => {
+              setPassword(value);
+            }}
+            placeholder='**********'
+          />
+        </label>
+        <button
+          type='submit'
+          disabled={disabledButton}
+        >
+          CADASTRAR
+        </button>
+      </form>
+      <div>
+        {failedTryRegister ? (
+          <p>Você já está cadastrado em nosso banco de dados.</p>
+        ) : null}
+      </div>
+    </main>
   );
 }
