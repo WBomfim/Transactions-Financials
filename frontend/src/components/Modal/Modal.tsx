f0d9d155f819d9443898843a47b08b85a22a429f
WBomfim <willianbomfim@hotmail.com>
2022-11-20 23:04:35 -0300

    feat: Criado um modal para a aplicação

---
+++ frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Modal.css'
+
+type ModalProps = {
+  children: React.ReactNode;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  show: boolean;
+};
+
+export default function Modal({ children, setShow, show }: ModalProps): JSX.Element {
+  return (
+    <div className='modal'>
+      <button
+        className='modal-close'
+        onClick={ () => setShow(!show) }
+      >
+        X
+      </button>
+      <div className='modal-main'>
+        <div className='modal-children'>
+          { children }
+        </div>
+      </div>
+    </div>
+  )
+} 
