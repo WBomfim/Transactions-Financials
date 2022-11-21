5505511b1b4e81761c00b16ef9b3574319275431
WBomfim <willianbomfim@hotmail.com>
2022-11-21 01:01:49 -0300

    feat: Criado o componente FilterByDate - Quando chamado o usuário pode fazer o filtro das transações por data e ordenar por tipo de transação.

---
+++ frontend/src/components/FilterByDate/FilterByDate.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { setToken, requestData } from '../../helpers/handleRequests';
+import { getLogin } from '../../helpers/handleStorage';
+import { Transaction } from '../StatementTable/StatementTable'
+
+export default function FilterByDate(
+  { setTransactions }: { setTransactions: React.Dispatch<React.SetStateAction<Transaction[] | []>> }
+): JSX.Element {
+  const [date, setDate] = useState<string>('');
+  const [typeFilter, setTypeFilter] = useState<string>('all');
+
+  const getTransactions = async (): Promise<void> => {
+    const { account } = getLogin() as { account: number };
+    setToken();
+    const transactions = await requestData(`/transactions/date?q=${date}`) as Transaction[];
+    if (typeFilter === 'credit') {
+      const transactionsCredit = transactions
+        .filter(({ creditedAccountId }) => creditedAccountId === account);
+      return setTransactions(transactionsCredit);
+    }
+    if (typeFilter === 'debit') {
+      const transactionsDebit = transactions
+        .filter(({ debitedAccountId }) => debitedAccountId === account);
+      return setTransactions(transactionsDebit);
+    }
+    return setTransactions(transactions);
+  };
+
+  return (
+    <>
+      <h2>Selecione a data desejada</h2>
+      <div>
+        <input
+          type="date"
+          value={ date }
+          onChange={ (e) => setDate(e.target.value) }
+        />
+        <div>
+          <label htmlFor="all">
+            <input
+              id="all"
+              type="radio"
+              name='typeTransactio'
+              checked={ typeFilter === 'all' }
+              onChange={ () => setTypeFilter('all') }
+            />
+            Todas
+          </label>
+          <label htmlFor="credit">
+            <input
+              id="credit"
+              type="radio"
+              name='typeTransactio'
+              checked={ typeFilter === 'credit' }
+              onChange={ () => setTypeFilter('credit') }
+            />
+            Crédito
+          </label>
+          <label htmlFor="debit">
+            <input
+              id="debit"
+              type="radio"
+              name='typeTransactio'
+              checked={ typeFilter === 'debit' }
+              onChange={ () => setTypeFilter('debit') }
+            />
+            Débito
+          </label>
+        </div>
+        <button
+          type="button"
+          onClick={ getTransactions }
+        >
+          Filtrar
+        </button>
+      </div>
+    </>
+  );
+};
